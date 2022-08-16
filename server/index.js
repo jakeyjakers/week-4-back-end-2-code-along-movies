4c162b16ebef479857edb9796f79e42512bac1de
Jacob Eichler <jeichler7@gmail.com>
2022-08-16 14:08:18 -0600

    Inital commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+//middleware
+app.use(express.json())
+app.use(cors())
+
+///////////----endpoints------/////////
+const {getMovies} = require('./controller.js')
+const {deleteMovie, createMovie, updateMovie} = require('./controller.js')
+
+
+app.get('/api/movies', getMovies)
+
+app.delete('/api/movies/:id', deleteMovie )
+
+app.post('/api/movies', createMovie)
+
+app.put('/api/movies/:id', updateMovie)
+
+
+
+/////////
+app.listen(4004, () => console.log('Running on port 4004') )
